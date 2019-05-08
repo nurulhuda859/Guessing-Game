c59923a3e5012cdb6de23cac62c60b7a67f0cd70
Nurul Huda <nurulhuda859g@gmail.com>
2019-05-08 10:51:17 +0600

    initial commit

--- app/src/main/java/me/nhas/guesinggame/MainActivity.java
+++ app/src/main/java/me/nhas/guesinggame/MainActivity.java
@@ -13,27 +13,51 @@ import java.lang.Integer;
 public class MainActivity extends AppCompatActivity {
 
     int number = (int) (Math.random() * 10 + 1);
-
+    int level = 1;
     public void guess (View view) {
+        TextView levelView = (TextView) findViewById(R.id.levelTextView);
+
         EditText guessText = (EditText) findViewById(R.id.guessEditText);
 
 
-        Log.i("info", guessText.getText().toString());
         Log.i("num", Integer.toString(number));
 
         int guessInt = Integer.parseInt(guessText.getText().toString());
 
+        if (level == -5) {
+            Toast.makeText(this, "ALAS YOU LOST!!", Toast.LENGTH_LONG).show();
+            level = 1;
 
-        if (guessInt == number ) {
+        }
+        else if (level == 5) {
+            Toast.makeText(this, "CONGRATS YOU WON!!", Toast.LENGTH_LONG).show();
+            level = 1;
+        }
+        else if (guessInt == number ) {
+            level = (int) level+1;
+            Log.i("lvl", Integer.toString(level));
+            levelView.setText(Integer.toString(level));
             Toast.makeText(this, "Matched", Toast.LENGTH_SHORT).show();
             number = (int) (Math.random() * 10 + 1);
             guessText.setText("");
         }
         else if (guessInt > number) {
+
+            level = (int) level-1;
+            Log.i("lvl", Integer.toString(level));
+            levelView.setText(Integer.toString(level));
+
+
             guessText.setText("");
             Toast.makeText(this, "Try lower", Toast.LENGTH_SHORT).show();
         }
         else if (guessInt < number) {
+
+            level = (int) level-1;
+            Log.i("lvl", Integer.toString(level));
+            levelView.setText(Integer.toString(level));
+
+
             guessText.setText("");
 
             Toast.makeText(this, "Try higher", Toast.LENGTH_SHORT).show();
